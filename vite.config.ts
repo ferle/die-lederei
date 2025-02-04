47cb72238e51404bbf9166de92c53207cb8db97d
Manuel <ferlitsch.manuel@gmail.com>
2025-02-04 11:00:55 +0100

    Updated Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change Vite's default output directory to 'build'
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
